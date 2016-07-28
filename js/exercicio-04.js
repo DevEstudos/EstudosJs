50b3a919eb039d139aed42c7e96105e691ff6967
Cauê Amaral <caue.amaral@hotmail.com>
2016-07-27 23:33:48 -0300

    Dica para fazer a função galeriaAtivo()

--- js/exercicio-04.js
+++ js/exercicio-04.js
@@ -1,2 +1,11 @@
 // Tudo é um objeto no JavaScript. Eu sei que eu objeto do tipo Node List tem um propriedade chamada "length" ou "comprimento" em português
 // console.log( galeriaFotos.length );
+
+
+// $('.galeria-foto').addClass('galeria-foto');
+
+// document.querySelector('.galeria-foto').classList.add('galeria-ativo');
+
+// document.querySelector('.galeria-foto').classList.remove('galeria-ativo');
+
+// document.querySelector('.galeria-foto').classList.contains('galeria-ativo');
