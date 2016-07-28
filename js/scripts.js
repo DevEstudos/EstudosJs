08437b8d4068e1cc3df9ea3e2d51061cfd5b416c
Cauê Amaral <caue.amaral@hotmail.com>
2016-07-27 23:23:48 -0300

    Funcionando com numeração

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,49 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-  var galeriaDestaque = document.querySelector('.galeria-destaque');
-  var galeriaFotos    = document.querySelectorAll('.galeria-foto');
+  /* ----- variaveis ----- */
+  var destaque = document.querySelector('.galeria-destaque');
+  var fotos    = document.querySelectorAll('.galeria-foto');
 
+  var numeracaoAtual = document.querySelector('.galeria-numeracao-atual');
+  var numeracaoTotal = document.querySelector('.galeria-numeracao-total');
+
+
+
+  /* ----- funcoes ---- */
   function galeriaTrocar() {
-    var galeriaClicado = this.getAttribute('src');
+    var clicado = this.getAttribute('src');
 
-    galeriaDestaque.setAttribute('src', galeriaClicado);
+    destaque.setAttribute('src', clicado);
   }
 
-  for (var i = 0; i < galeriaFotos.length; i++) {
-    galeriaFotos[i].addEventListener('click', galeriaTrocar);
+  function galeriaNumeracaoDefinicao(imagem, posicao) {
+    var posicao = posicao + 1;
+
+    imagem.setAttribute('data-numeracao', posicao);
   }
+
+  function galeriaNumeracaoTotal() {
+    var total = fotos.length;
+
+    numeracaoTotal.innerHTML = total;
+  }
+
+  function galeriaNumeracaoAtual() {
+    var atual = this.getAttribute('data-numeracao');
+
+    numeracaoAtual.innerHTML = atual;
+  }
+
+
+
+  /* ----- galeria em funcionamento ----- */
+  for (var i = 0; i < fotos.length; i++) {
+    galeriaNumeracaoDefinicao(fotos[i], i);
+
+    fotos[i].addEventListener('click', galeriaTrocar);
+    fotos[i].addEventListener('click', galeriaNumeracaoAtual);
+  }
+
+  galeriaNumeracaoTotal();
 
 });
